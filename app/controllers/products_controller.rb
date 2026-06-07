8647cb4664fb3beb2875601caf0889bbc9c31313
YuSuf_Abdelfattah <97110015+YUSUF-SELEIM@users.noreply.github.com>
2026-06-07 13:02:46 +0000

    Implmented auth + products model, route and views + migrated db

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,50 @@
+class ProductsController < ApplicationController
+  before_action :require_authentication
+  allow_unauthenticated_access only: [:index, :show]
+  
+  def index
+    @products = Product.all.order(created_at: :desc)
+  end
+
+  def show
+    @product = Product.find(params[:id])
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to @product, notice: "Product created!"
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @product = Product.find(params[:id])
+  end
+
+  def update
+    @product = Product.find(params[:id])
+    if @product.update(product_params)
+      redirect_to @product, notice: "Product updated!"
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @product = Product.find(params[:id])
+    @product.destroy
+    redirect_to products_path, notice: "Product deleted!"
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:name, :price)
+  end
+end
