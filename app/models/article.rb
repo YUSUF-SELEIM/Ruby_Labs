0287a065a78601969d0897ff743ca7a33692aa1f
YuSuf_Abdelfattah <97110015+YUSUF-SELEIM@users.noreply.github.com>
2026-06-08 12:45:45 +0000

    edited articles + user models

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,22 @@
 class Article < ApplicationRecord
   belongs_to :user
+  has_one_attached :image
+  enum :status, { public_article: 0, archived: 1 }
+  validates :title, presence: true
+  validates :body, presence: true
+  after_initialize :set_defaults, if: :new_record?
+  before_save :check_reports
+
+  private
+
+  def set_defaults
+    self.reports_count ||= 0
+    self.status ||= :public_article
+  end
+
+  def check_reports
+    if reports_count >= 3
+      self.status = :archived
+    end
+  end
 end
